f687d075ae7a32563d8662782cc6f8d5961fc02f
Chaiku <46286216+Chaiku@users.noreply.github.com>
2019-02-25 20:10:03 -0500

    added a functioning cart modal.

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,9 @@ $(function () {
     let cartArray = [];
 
     const render = function(products) {
+        $('#prodTable').empty();
         for (i = 0; i < products.length; i++) {
-            $('tbody').append(`
+            $('#prodTable').append(`
                 <tr>
                     <th scope="row" class="productID">${products[i].id}</th>
                     <td class="product_name">${products[i].product_name}</td>
@@ -12,7 +13,7 @@ $(function () {
                     <td class="price">${products[i].price}</td>
                     <td class="stock_quantity">${products[i].stock_quantity}</td>
                     <td class="purchase_quantity"><input id="qtyId${products[i].id}" data-prodID="${products[i].id}" type="number" name="quantity" min="0" max="${products[i].stock_quantity}"></input></td>
-                    <td class="addCart"><button class="btn-success addToCart" id="addId${products[i].id}" data-prodID="${products[i].id}" data-stockQty="${products[i].stock_quantity}">Add to Cart</button></td>
+                    <td class="addCart"><button class="btn-success addToCart" id="addId${products[i].id}" data-prodID="${products[i].id}" data-stockQty="${products[i].stock_quantity}" data-prodName="${products[i].product_name}" data-price="${products[i].price}">Add to Cart</button></td>
                 </tr>
             `);
             document.getElementById(`addId${products[i].id}`).addEventListener("click", addToCart);
@@ -26,7 +27,6 @@ $(function () {
             url: '/api/products',
         }).then(function (data) {
             console.log(data);
-            $('tbody').empty();
             render(data);
         });
         console.log("I worked");
@@ -40,17 +40,21 @@ $(function () {
 
         console.log("This is working");
         let updateID = $(this).attr('data-prodID');
-        console.log(updateID);
+        let purchaseName = $(this).attr('data-prodName');
         let totalQty = $(this).attr(`data-stockQty`);
-        console.log(totalQty);
+        let purcPrice = $(this).attr(`data-price`);
         let subtractQty = ($(`#qtyId${updateID}`).val());
-        console.log(subtractQty);
         
 
     if (totalQty >= subtractQty && totalQty !== 0) {
         let updatedQty = totalQty - subtractQty;
         console.log(updatedQty);
-
+        const newPurchase = {
+            purchaseName: purchaseName,
+            purchaseQty: subtractQty,
+            purchasePrice: purcPrice * subtractQty
+        }
+        cartArray.push(newPurchase);
         $.ajax({
             method: 'PUT',
             url: `/api/products/${updateID}`,
@@ -61,20 +65,31 @@ $(function () {
             console.log(data);
             $(`#qtyId${updateID}`).val("");
             renderProducts();
+           
         });
         } else {
             alert("We do not have any more in stock.");
         }
-    
     
     };
 
 
 
 
-    // const viewCart = function() {
+    const viewCart = function(event) {
+        event.preventDefault();
+        $('#cart').empty();
+        for (i = 0; i < cartArray.length; i++) {
+            $('#cart').append(`
+                <tr>
+                    <th scope="row" class="cartProd">${cartArray[i].purchaseName}</th>
+                    <td class="cartQty">${cartArray[i].purchaseQty}</td>
+                    <td class="cartPrice">${cartArray[i].purchasePrice}</td>
+                <tr>
+            `)
+        }
 
-    // }
+    }
 
    
 
@@ -83,7 +98,7 @@ $(function () {
 
 
 
-    // $('#viewCartBtn').on("click", viewCart)
+    $('#checkout').on("click", viewCart)
 
 
 });
